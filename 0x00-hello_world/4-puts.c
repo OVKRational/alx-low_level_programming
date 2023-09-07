24d146c60aaec89d6e2f93d144e1ea66f1353263
OVKRational <oparevictorkofi@gmail.com>
2023-09-07 13:45:57 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main a programe that print a line with put function
+ *
+ *  return 0
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
