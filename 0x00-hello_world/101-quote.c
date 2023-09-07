dced8c12a6a9facc44cc3ecbcb1e8179211d3d83
OVKRational <oparevictorkofi@gmail.com>
2023-09-07 16:49:16 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - a c programe that print a line to the standard error
+ * main - main function
  *
  * Return: 1 (successful)
  */
 int main(void)
 {
 	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			
-			return (1);
+
+	return (1);
 }
