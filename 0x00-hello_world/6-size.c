1df8f2d833ce76b7ee71dfcb336d7b7593b869eb
OVKRational <oparevictorkofi@gmail.com>
2023-09-07 15:14:44 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - a programe that print the size of various computer types
+ * Return: 0 (return 0 means success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char %lu byte\n",(unsigned long)
+			sizeof[a]);
+	printf("Size of an int %lu byte\n",(unsigned long)
+                        sizeof[b]);
+	printf("Size of a long int %lu byte\n",(unsigned long)
+                        sizeof[c]);
+	printf("Size of a long long int %lu byte\n",(unsigned long)
+                        sizeof[d]);
+	printf("Size of a float %lu byte\n",(unsigned long)
+                        sizeof[f]);
+	return (0);
+}
