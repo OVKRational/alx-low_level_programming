b8cd345ce6291471075bb2838bacb11a0d858158
OVKRational <oparevictorkofi@gmail.com>
2023-09-07 14:16:57 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
- *  main a programe that print a line with put function
+ *  main - a programe that print a line with put function
  *
- *  return 0
+ * Description: this function prints using the puts function and return 0.
+ *
+ *  return 0 (return 0 means success)
  */
 int main(void)
 {
-	printf("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
