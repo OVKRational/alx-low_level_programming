ebbdf482f85a7183010ca2d6c6389486bf419bcd
OVKRational <oparevictorkofi@gmail.com>
2023-09-09 15:50:52 -0700

    print number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio>
+#include <stdlib.h>
+
+/**
+ * main - Prints numbers
+ *
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
