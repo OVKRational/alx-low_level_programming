6ce63cffa8ebac25eed99e5e21cf6343b6f2e60d
OVKRational <oparevictorkofi@gmail.com>
2023-09-11 11:37:17 -0700

    print comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+int numLeft;
+int numLast;
+
+for (numLast = 0; numLeft < 100; numLeft++)
+{
+for (numLast = 0; numLeft < 100; numLast++)
+{
+if (numLeft < numLast)
+{
+putchar(numLeft / 10 + 48);
+putchar(numLeft % 10 + 48);
+putchar(32);
+putchar(numLast / 10 + 48);
+putchar(numLast % 10 + 48);
+if (numLeft < 98)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
