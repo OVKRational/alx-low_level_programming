0f10fad6f7e52e6f79ec5593b3a6c02d78dd0ad7
OVKRational <oparevictorkofi@gmail.com>
2023-09-11 11:59:55 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdilb.h>
 #include <time.h>
+
 /**
  * main - Print the alphabet in lowercase, followed by a new line
  *
  * Return: Always 0(success)
  */
+
 int main(void)
 {
 	char letter;
