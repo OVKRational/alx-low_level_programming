909c3cd5077bf4dcd10a03f0abdade878d706717
OVKRational <oparevictorkofi@gmail.com>
2023-09-13 17:11:45 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,9 +16,10 @@ void jack_bauer(void)
 	{
 	_putchar(i / 10 + '0');
 	_putchar(i % 10 + '0');
-	_putchar('58');
+	_putchar(':');
 	_putchar(j / 10 + '0');
 	_putchar(j % 10 + '0');
+	_putchar('\n');
 	}
 	}
 }
