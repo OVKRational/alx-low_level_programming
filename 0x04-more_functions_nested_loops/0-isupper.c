ac43ff5ef6268900ffd6aa6e71af2c41911e31a9
OVKRational <oparevictorkofi@gmail.com>
2023-09-15 03:55:21 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks the uppercase character
+ * @c: functiona parameter
+ *
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (C >= 'A' && C <= 'Z')
+		return (1);
+	else
+		return (0);
+}
