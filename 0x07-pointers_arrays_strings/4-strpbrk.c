f1bc246ee02c00b6e2f75282a1a68e4da5b7da69
OVKRational <oparevictorkofi@gmail.com>
2023-09-26 16:28:04 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - entry point
+ * @s: input value
+ * @accept: input value
+ *
+ * Return: 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+				return (0);
+		}
+		s++;
+	}
+	return ('\0');
