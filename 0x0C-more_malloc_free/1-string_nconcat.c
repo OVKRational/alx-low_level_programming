c582861937962cc0135c5a3657b2d4558ff1284d
OVKRational <oparevictorkofi@gmail.com>
2023-10-08 12:57:01 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,31 +13,31 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i = 0, j = 0, lenl = 0, len2 = 0;
+	unsigned int i = 0, j = 0, ovkl = 0, ovk2 = 0;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
+	while (s1 && s1[ovk1])
+		ovk1++;
+	while (s2 && s2[ovk2])
+		ovk2++;
 
-	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
+	if (n < ovk2)
+		s = malloc(sizeof(char) * (ovk1 + n + 1));
 	else
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
+		s = malloc(sizeof(char) * (ovk1 + ovk2 + 1));
 
 	if (!s)
 		return (NULL);
 
-	while (i < len1)
+	while (i < ovk1)
 	{
 		s[i] = s1[i];
 		i++;
 	}
 
-	while (n < len2 && i < (len1 + n))
+	while (n < ovk2 && i < (ovk1 + n))
 		s[i++] = s2[j++];
 
-	while (n >= len2 && i < (len1 + len2))
+	while (n >= ovk2 && i < (ovk1 + ovk2))
 		s[i++] = s2[j++];
 
 	s[i] = '\0';
